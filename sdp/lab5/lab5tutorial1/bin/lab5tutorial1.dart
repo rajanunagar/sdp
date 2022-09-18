07b1dd8644a9c8425e3e20cf86b58ba3d0247a00
rajanunagar <86923104+rajanunagar@users.noreply.github.com>
2022-09-18 15:23:25 +0530

    Update lab5tutorial1.dart

--- sdp/lab5/lab5tutorial1/bin/lab5tutorial1.dart
+++ sdp/lab5/lab5tutorial1/bin/lab5tutorial1.dart
@@ -3,7 +3,7 @@ import 'package:lab5_tutorial1/lab5_tutorial1.dart' as lab5_tutorial1;
 void main(List<String> arguments) {
   // print('Hello world: ${lab5_tutorial1.calculate()}!');
 
-  // Basic list operations
+  //some  Basic list operations
 
 /*
  // Creating a list => using '[]'
